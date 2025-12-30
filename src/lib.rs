e4d0ef0bef5ec9472e8d0713af88cba475a61256
Luracasmus <77991691+Luracasmus@users.noreply.github.com>
2025-12-30 16:26:14 +0100

    Make naming consistent (mcshader everywhere)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-use zed_extension_api::{self as zed, Result};
+use zed_extension_api::{self as zed, Result, serde_json, settings::LspSettings};
 
 struct McshaderExtension;
 
 impl McshaderExtension {
-    const LANGUAGE_SERVER_ID: &'static str = "vscode-mcshader";
+    const LANGUAGE_SERVER_ID: &'static str = "mcshader";
 
     fn language_server_binary_path(&mut self, worktree: &zed::Worktree) -> Result<String> {
         worktree
             .which(Self::LANGUAGE_SERVER_ID)
-            .ok_or(String::from("vscode-mcshader LSP not found"))
+            .ok_or(String::from("mcshader LSP not found"))
 
         // TODO: Download the server or something if it doesn't exist.
     }
@@ -27,8 +27,23 @@ impl zed::Extension for McshaderExtension {
         Ok(zed::Command {
             command: self.language_server_binary_path(worktree)?,
             args: vec![],
-            env: Default::default(),
+            env: vec![(String::from("RUST_BACKTRACE"), String::from("1"))],
         })
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<Option<serde_json::Value>, String> {
+        let settings = LspSettings::for_worktree(Self::LANGUAGE_SERVER_ID, worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+
+        Ok(Some(serde_json::json!({
+            Self::LANGUAGE_SERVER_ID: settings
+        })))
     }
 }
 
