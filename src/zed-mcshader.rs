361b74051f36efe997a233b42791555b83cee584
Luracasmus <77991691+Luracasmus@users.noreply.github.com>
2025-12-30 14:10:24 +0100

    Initial commit

---
+++ src/zed-mcshader.rs
@@ -0,0 +1,33 @@
+use zed_extension_api::{self as zed, Result};
+
+struct McshaderExtension;
+
+impl McshaderExtension {
+    fn language_server_binary_path(&mut self, worktree: &zed::Worktree) -> Result<String> {
+        worktree
+            .which("vscode-mcshader")
+            .ok_or(String::from("vscode-mcshader LSP not found"))
+
+        // TODO: Download the server or something if it doesn't exist.
+    }
+}
+
+impl zed::Extension for McshaderExtension {
+    fn new() -> Self {
+        Self
+    }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        Ok(zed::Command {
+            command: self.language_server_binary_path(worktree)?,
+            args: vec![],
+            env: Default::default(),
+        })
+    }
+}
+
+zed::register_extension!(McshaderExtension);
